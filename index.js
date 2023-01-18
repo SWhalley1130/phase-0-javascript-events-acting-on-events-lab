4ab51d3ee854fcbd603981bff834f2fa50ab3508
Sarah Whalley <s.whalley1130@gmail.com>
2023-01-18 10:56:01 -0700

    Adds right function and event listener

--- index.js
+++ index.js
@@ -12,10 +12,29 @@ function moveDodgerLeft()
     }
   }
 
+  function moveDodgerRight()
+  {
+    const rightNumbers=dodger.style.left.replace("px","");
+    const right =parseInt(rightNumbers,10);
+    
+    if (right<360)
+    {
+        dodger.style.left=`${right+1}px`;
+    }    
+     
+    
+  }
+
   document.addEventListener("keydown", function (e) 
   {
+    
     if (e.key === "ArrowLeft") 
     {
       moveDodgerLeft();
     }
+    else if (e.key==="ArrowRight")
+    {
+        moveDodgerRight();
+    }
   });
+
